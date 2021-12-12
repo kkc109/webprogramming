9bf344b76fda3ac8c7adb62c20bbde3804ab579c
kkc109 <83759793+kkc109@users.noreply.github.com>
2021-12-12 23:18:19 +0900

    Add files via upload

---
+++ information.js
@@ -0,0 +1,7 @@
+function openclose(element){
+    if(document.getElementById('element').style.display === 'none') {
+        document.getElementById('element').style.display = 'flex';
+    } 
+    else {
+    document.getElementById('element').style.display = 'none';}
+}
---
+++ nav.js
@@ -0,0 +1,8 @@
+function openCloseToc() {
+    if(document.getElementById('link').style.display === 'none') {
+        document.getElementById('link').style.display = 'flex';
+    } 
+    else {
+    document.getElementById('link').style.display = 'none';
+    }
+ }
